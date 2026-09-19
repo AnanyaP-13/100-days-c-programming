4e367e13f509b3630e03998e99f4985553f0da30
Ananya <ananya.33797@stu.upes.ac.in>
2026-09-19 23:45:25 +0530

    Add files via upload

---
+++ ques81.c
@@ -0,0 +1,18 @@
+//Q81: Count characters in a string without using built-in length functions.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    scanf("%s", str);
+
+    while(str[count] != '\0') {
+        count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
