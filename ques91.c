70b95789c75db5944732c833ed2e56f26703e9ff
Ananya <ananya.33797@stu.upes.ac.in>
2026-09-24 22:22:54 +0530

    Add files via upload

---
+++ ques91.c
@@ -0,0 +1,23 @@
+//Q91: Remove all vowels from a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    scanf("%s", str);
+
+    while(str[i] != '\0') {
+
+        if(str[i] != 'a' && str[i] != 'e' &&
+           str[i] != 'i' && str[i] != 'o' &&
+           str[i] != 'u') {
+            printf("%c", str[i]);
+        }
+
+        i++;
+    }
+
+    return 0;
+}
